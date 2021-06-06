cb3c9534f066828f6263037e3850f348afdcb9d0
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-06 12:21:34 +0530

    Added : Welcome message display

---
+++ EmployeeWageComputation/src/empwage/WageCalculator.java
@@ -0,0 +1,18 @@
+package empwage;
+
+public class WageCalculator {
+	
+	/**
+	 * Displays Welcome message on console
+	 */
+	private void displayWelcome() {
+		System.out.println("Welcome to Employee Wage calculator");
+	}
+
+	public static void main(String[] args) {
+		WageCalculator w =new WageCalculator();
+		w.displayWelcome();
+
+	}
+
+}
