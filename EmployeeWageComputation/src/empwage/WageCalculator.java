733f26d1642a9d19b943385e41e6b72a487dc6d9
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-06 15:26:02 +0530

    Added : Calc wage till a condition is reached

--- EmployeeWageComputation/src/empwage/WageCalculator.java
+++ EmployeeWageComputation/src/empwage/WageCalculator.java
@@ -5,7 +5,7 @@ public class WageCalculator {
 	private final int IS_ABSENT=0;
 	private static final int IS_FULLTIME=1;
 	private static final int IS_PARTTIME=2;
-	private final int WORK_DAYS_IN_MONTH=20;
+	private static final int WORK_DAYS_IN_MONTH=20;
 	private final int WAGE_PER_HR=20;
 	private int dayHrs;
 	
@@ -51,27 +51,49 @@ public class WageCalculator {
 		int monthlyWage = WORK_DAYS_IN_MONTH*WAGE_PER_HR*dayHrs;
 		return monthlyWage;
 	}
+	/**
+	 * @return Total wage 
+	 * Calculating wages till number of hrs reaches 100 or Working days reaches 20
+	 */
+	private static int calcWages() {
+		int ran;
+		int TotalWage=0;
+		int countHrs=0;
+		int countDays=0;
+		WageCalculator fullTimeEmp1 =new WageCalculator(8);
+		WageCalculator partTimeEmp1=new WageCalculator(4);
+		Random random=new Random();
+		ran=1+random.nextInt(2);
+		while(countHrs<100 && countDays<WORK_DAYS_IN_MONTH)
+		{
+			switch (ran) {
+    		case IS_FULLTIME:
+    			TotalWage+=fullTimeEmp1.calcDailyWage();
+    			countHrs+=8;
+    			countDays++;
+    			break;
+    		case IS_PARTTIME:
+    			TotalWage+=partTimeEmp1.calcDailyWage();
+    			countHrs+=4;
+    			countDays++;
+    			break;
+    	    }
+			
+		}
+		return TotalWage;
+	}
 	
 	public static void main(String[] args) {
 		displayWelcome();
 		WageCalculator fullTimeEmp =new WageCalculator(8);
 		WageCalculator partTimeEmp=new WageCalculator(4);	
-		Random random=new Random();
 		fullTimeEmp.attendence();
 		partTimeEmp.attendence();
-		int ran;
-		ran=1+random.nextInt(2);
-		switch (ran) {
-    	case IS_FULLTIME:
-    		fullTimeEmp.calcDailyWage();
-    		fullTimeEmp.calcMonthlyWage();
-    		break;
-    	case IS_PARTTIME:
-    		partTimeEmp.calcDailyWage();
-    		partTimeEmp.calcMonthlyWage();
-    		break;
-    	}
-		
+		fullTimeEmp.calcDailyWage();
+		partTimeEmp.calcDailyWage();
+		fullTimeEmp.calcMonthlyWage();
+		partTimeEmp.calcMonthlyWage();
+		calcWages();				
 	}
 
 }
