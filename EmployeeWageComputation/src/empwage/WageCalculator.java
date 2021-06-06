329d2993a03096f09c0d50d1d065241ded3596ab
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-06 12:35:00 +0530

    Added : attendence()

--- EmployeeWageComputation/src/empwage/WageCalculator.java
+++ EmployeeWageComputation/src/empwage/WageCalculator.java
@@ -1,6 +1,7 @@
 package empwage;
-
+import java.util.Random;
 public class WageCalculator {
+	private final int IS_PRESENT=1;
 	
 	/**
 	 * Displays Welcome message on console
@@ -8,10 +9,24 @@ public class WageCalculator {
 	private void displayWelcome() {
 		System.out.println("Welcome to Employee Wage calculator");
 	}
+	/**
+	 * @return 1 if employee is present otherwise 0
+	 */
+	private int attendence() {
+    	int ran;
+    	Random random=new Random();
+    	ran=random.nextInt(2);
+		System.out.println(ran);
+		if(ran==IS_PRESENT)
+			return 1;
+		else
+			return 0;
+   }
 
 	public static void main(String[] args) {
 		WageCalculator w =new WageCalculator();
 		w.displayWelcome();
+		w.attendence();
 
 	}
 
