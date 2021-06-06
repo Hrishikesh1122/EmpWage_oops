1028dd24c018074d2997f1daedd1673a000c3c4b
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-06 12:53:39 +0530

    Added : Daily wage calculation

--- EmployeeWageComputation/src/empwage/WageCalculator.java
+++ EmployeeWageComputation/src/empwage/WageCalculator.java
@@ -2,6 +2,9 @@ package empwage;
 import java.util.Random;
 public class WageCalculator {
 	private final int IS_PRESENT=1;
+	private final int IS_ABSENT=0;
+	private final int WAGE_PER_HR=20;
+	private final int FULL_DAY_HRS=8;
 	
 	/**
 	 * Displays Welcome message on console
@@ -18,15 +21,23 @@ public class WageCalculator {
     	ran=random.nextInt(2);
 		System.out.println(ran);
 		if(ran==IS_PRESENT)
-			return 1;
+			return IS_PRESENT;
 		else
-			return 0;
-   }
+			return IS_ABSENT;
+    }
+	/**
+	 * @return Wage per day
+	 */
+	private int calcDailyWage() {
+	    	int dailyWage = WAGE_PER_HR*FULL_DAY_HRS;
+	    	return dailyWage;
+	}
 
 	public static void main(String[] args) {
 		WageCalculator w =new WageCalculator();
 		w.displayWelcome();
 		w.attendence();
+		w.calcDailyWage();
 
 	}
 
